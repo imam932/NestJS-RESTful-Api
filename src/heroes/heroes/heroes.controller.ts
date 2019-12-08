bf69111c3fb94925c6429b4cdadf8ea746f46c2a
imam932 <nawawi932@gmail.com>
2019-12-08 15:58:32 +0700

    Add Endpoint Update, Delete

--- src/heroes/heroes/heroes.controller.ts
+++ src/heroes/heroes/heroes.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common'
 import { promises } from 'fs';
 import { Heroe } from './../heroe.entity';
 import { HeroesService } from './heroes.service';
+import { identifier } from '@babel/types';
 
 @Controller('heroes')
 export class HeroesController {
@@ -15,5 +16,17 @@ export class HeroesController {
     @Post()
     async create(@Body() heroeData: Heroe): Promise<any> {
         return this.heroesService.create(heroeData);
+    }
+
+    @Put(':id/update')
+    async update(@Param('id') id, @Body() heroeData: Heroe): Promise<any> {
+        heroeData.id = Number(id);
+        console.log('Update => '+ heroeData.id);        
+        return this.heroesService.update(heroeData);
+    }
+
+    @Delete(':id/delete')
+    async delete(@Param('id') id): Promise<any> {
+        return this.heroesService.delete(id);
     }
 }
